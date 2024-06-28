da92c4a5f153516668639252866a2c3dd2ccb96b
unknown <borodinov542@yandex.ru>
2024-06-29 00:23:58 +0700

    fix file path again

--- tools/ticked_file_enforcement/ticked_file_enforcement.py
+++ tools/ticked_file_enforcement/ticked_file_enforcement.py
@@ -66,8 +66,9 @@ with open(file_reference, 'r') as file:
     print(f"Getting module files from:")
     if scannable_directory == "massmeta/":
         for module_file in lines:
+            module_file_path = module_file.replace('/', '\\')
             print(f"    {module_file} with:")
-            with open(module_file, 'r') as extra_file:
+            with open(module_file_path, 'r') as extra_file:
                 for extra_line in extra_file:
                     # all include files in each module must be "includes.dm" = 11 length
                     lines.append(module_file[:(len(module_file) - 11)] + extra_line[10:])
