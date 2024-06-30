f04af1be9aacdb024bde0799d613b9d03edd1e80
unknown <borodinov542@yandex.ru>
2024-06-30 20:36:00 +0700

    make modular files go in order

--- tools/ticked_file_enforcement/ticked_file_enforcement.py
+++ tools/ticked_file_enforcement/ticked_file_enforcement.py
@@ -63,39 +63,36 @@ with open(file_reference, 'r') as file:
         # MASSMETA EDIT ADDITION END
         lines.append(line)
 
-# MASSMETA EDIT ADDITION START (check modular code folder)
-if scannable_directory == "massmeta/features/":
-    extra_lines = []
-    fail_no_include_modular = False
-    print(blue(f"Scanning Modular Code... Checking files in [{scannable_directory}]"))
-    for module_file in lines:
-        module_file_path = module_file.replace('\\', '/')
-        module_file_path_clean = module_file_path[10:-1]
-        print(f"    [{module_file_path_clean}] with in it:")
-        if module_file_path_clean[-11:] != "includes.dm":
-            red(f"      File [{module_file_path_clean}] must be named \"includes.dm\", skipping the file.")
-            fail_no_include_modular = True
-            continue
+        # MASSMETA EDIT ADDITION START (check modular code folder)
+        if scannable_directory == "massmeta/features/":
+            fail_no_include_modular = False
+            print(blue(f"Scanning Modular Code... Checking files in [{scannable_directory}]"))
 
-        with open(module_file_path_clean, 'r') as extra_file:
-            for extra_line in extra_file:
-                extra_line = extra_line.strip()
-                if (extra_line[10:14] != "code"):
-                    red(f"       File [{extra_line}] must be in \"code/\" folder")
-                    fail_no_include_modular = True
+            module_file_path = line.replace('\\', '/')
+            module_file_path_clean = module_file_path[10:-1]
+            print(f"    [{module_file_path_clean}] with in it:")
+            if module_file_path_clean[-11:] != "includes.dm":
+                red(f"      File [{module_file_path_clean}] must be named \"includes.dm\", skipping the file.")
+                fail_no_include_modular = True
+                continue
 
-                # make full path
-                extra_line = module_file[:-12] + extra_line[10:]
-                extra_lines.append(extra_line)
-                extra_line.replace('\\', '/')
-                print(f"        [{extra_line[10:-1]}]")
+            with open(module_file_path_clean, 'r') as extra_file:
+                for extra_line in extra_file:
+                    extra_line = extra_line.strip()
+                    if (extra_line[10:14] != "code"):
+                        red(f"       File [{extra_line}] must be in \"code/\" folder")
+                        fail_no_include_modular = True
 
-    if fail_no_include_modular:
-        post_error(f"Modular Ticked File Enforcement has failed!")
-        sys.exit(1)
+                    # make full path
+                    extra_line = line[:-12] + extra_line[10:]
+                    lines.append(extra_line)
+                    extra_line.replace('\\', '/')
+                    print(f"        [{extra_line[10:-1]}]")
 
-    lines.extend(extra_lines)
-# MASSMETA EDIT ADDITION END
+        if fail_no_include_modular:
+            post_error(f"Modular Ticked File Enforcement has failed!")
+            sys.exit(1)
+        # MASSMETA EDIT ADDITION END
 
 offset = total - len(lines)
 print(blue(f"Ticked File Enforcement: {offset} lines were ignored in output for [{file_reference}]."))
